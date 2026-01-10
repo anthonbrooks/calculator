eec18d96622f97821577472d6eced275e6c12307
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-10 12:20:19 -0700

    Add selectors for buttons

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// create variables for calculator buttons
+let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operator");
+let clear = document.querySelector("#clear");
+let negative = document.querySelector("#negative");
+let percent = document.querySelector("#percent");
+let equals = document.querySelector("#evaluate");
+let decimal = document.querySelector("#decimal");
+
 // add two numbers
 const add = (x, y) => x + y;
 
